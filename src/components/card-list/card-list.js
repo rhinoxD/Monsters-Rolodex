be550a6053358f7e938484817c0c947114a67faf
rhinoxD <coolshiv0721@gmail.com>
2022-01-11 12:42:57 +0530

    Card Component

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Card from '../card/card';
 import './card-list.styles.css';
+
 const CardList = (props) => {
-  console.log(props);
-  return <div className='card-list'>{props.children}</div>;
+  return (
+    <div className='card-list'>
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
